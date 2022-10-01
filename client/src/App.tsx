9d22b063147b87d15e987e1ff2aa491140baa700
Andrew Carnero <andrew.carnero@gmail.com>
2022-10-01 10:35:53 -0400

    [react] upgrade to 18.0.2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useId } from 'react';
 import AppProviders from './providers/AppProviders';
 
 function App() {
+  const id = useId();
+  console.log(id);
   return (
     <AppProviders>
       <div>Hello world!!!</div>
