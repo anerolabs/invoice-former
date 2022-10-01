7083c487ea159a8717c17bed0f45b7ba89983253
Andrew Carnero <andrew.carnero@gmail.com>
2022-10-01 11:53:42 -0400

    [app shell] create app wrappers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import AppProviders from './providers/AppProviders';
 
+import Box from '@mui/material/Box';
+
 import SpreadSheetLoader from './components/SpreadsheetLoader/SpreadsheetLoader';
 
 export default function App() {
   return (
     <AppProviders>
-      <div>Hello world!!!</div>
-      <SpreadSheetLoader />
+      <Box id="root-container" height="100vh" width="100vw" margin="auto">
+        <Box
+          id="app-shell"
+          height="100vh"
+          width="80vw"
+          margin="auto"
+          sx={{
+            padding: 2,
+          }}
+        >
+          <SpreadSheetLoader />
+        </Box>
+      </Box>
     </AppProviders>
   );
 }
